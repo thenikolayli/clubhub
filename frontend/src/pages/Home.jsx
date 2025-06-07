71bc7ca49f7987c9d36424aeee1bba7c69264882
thenikolayli <nikolay.li2008@gmail.com>
2025-06-07 00:16:14 -0700

    finish ui

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,76 @@
 import {createSignal, onMount} from "solid-js";
 import logo from "../assets/logo.png";
 import {ClubCard} from "../components/ClubCard.jsx";
+import gsap from "gsap";
 
 export const Home = () => {
     const [clubs, set_clubs] = createSignal([])
 
+    const [url, set_url] = createSignal("https://images.unsplash.com/photo-1749117631912-df9d281e9744")
+    const [name, set_name] = createSignal("clubhub")
+    const [description, set_description] = createSignal("wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww")
+
     onMount(() => {
         document.title = "clubhub"
+
+        gsap.set(".more-menu", {
+            opacity: 0,
+            display: "none"
+        })
+        gsap.set(".more-panel", {
+            opacity: 0,
+            display: "none"
+        })
     })
+
+    const toggle_menu = (purl, pname, pdescription, visible) => {
+        // update state
+        // toggle menu
+
+        const timeline = gsap.timeline({
+            defaults: {
+                duration: .4,
+                ease: "power2.out"
+            }
+        })
+
+        if (visible) {
+            set_url(purl)
+            set_name(pname)
+            set_description(pdescription)
+            gsap.set(".more-menu", {
+                display: "block"
+            })
+            gsap.set(".more-panel", {
+                display: "block"
+            })
+
+            timeline.to(".more-menu", {
+                opacity: 1
+            }, 0)
+            timeline.to(".more-panel", {
+                opacity: 1
+            }, .1)
+        } else {
+            timeline.to(".more-menu", {
+                opacity: 0
+            }, 0).then(() => {
+                gsap.set(".more-menu", {
+                    display: "none"
+                })
+            })
+            timeline.to(".more-panel", {
+                opacity: 0
+            }, 0).then(() => {
+                gsap.set(".more-menu", {
+                    display: "none"
+                })
+                set_url(purl)
+                set_name(pname)
+                set_description(pdescription)
+            })
+        }
+    }
 
     return (
         <>
@@ -30,14 +93,29 @@ export const Home = () => {
 
                     <div class="grid grid-cols-2 gap-4">
                         <ClubCard name={"jit club"} description={"wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww"}
+                                  url={"https://images.unsplash.com/photo-1749117631912-df9d281e9744"} toggle_menu={toggle_menu}/>
+                        <ClubCard name={"jjj club"} description={"clubhubssdfgasdfasfgsdfgsdfgsdfgsdfgsdfg"}
                                   url={"https://images.unsplash.com/photo-1749117631912-df9d281e9744"} />
-                        <ClubCard name={"jit club"} description={"clubhubssdfgasdfasfgsdfgsdfgsdfgsdfgsdfg"}
-                                  url={"https://images.unsplash.com/photo-1749117631912-df9d281e9744"} />
-                        <ClubCard name={"jit club"} description={"clubhubssdfgasdfasfgsdfgsdfgsdfgsdfgsdfg"}
-                                  url={"https://images.unsplash.com/photo-1749117631912-df9d281e9744"} />
-                        <ClubCard name={"jit club"} description={"clubhubssdfgasdfasfgsdfgsdfgsdfgsdfgsdfg"}
-                                  url={"https://images.unsplash.com/photo-1749117631912-df9d281e9744"} />
+                        {/*<ClubCard name={"jit club"} description={"clubhubssdfgasdfasfgsdfgsdfgsdfgsdfgsdfg"}*/}
+                        {/*          url={"https://images.unsplash.com/photo-1749117631912-df9d281e9744"} />*/}
+                        {/*<ClubCard name={"jit club"} description={"clubhubssdfgasdfasfgsdfgsdfgsdfgsdfgsdfg"}*/}
+                        {/*          url={"https://images.unsplash.com/photo-1749117631912-df9d281e9744"} />*/}
                     </div>
+                </div>
+            </section>
+
+            <button class={"fixed top-0 left-0 more-menu w-screen h-screen z-30 backdrop-blur"}
+                    onclick={() => toggle_menu("", "", "", false)}
+            />
+
+            <section class={"absolute inset-x-0 mx-auto top-10 more-panel w-[45%] z-40 border-4 rounded-xl border-cwhite bg-cwhite"}>
+                <div class="w-full h-[20rem] rounded-xl border-4 overflow-hidden border-cwhite">
+                    <img class={"object-cover"} src={url()} alt={name()}/>
+                </div>
+
+                <div class="w-full rounded-xl border-4 p-2 border-cwhite bg-corange break-words">
+                    <header class={"text-4xl"}>{name()}</header>
+                    <h1 class={"text-xl"}>{description()}</h1>
                 </div>
             </section>
         </>
